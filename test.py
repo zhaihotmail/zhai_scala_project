478e487e2dc9698d13c0890f06e42c6c1750cf22
zhaihotmail <zhaizhongping@hotmail.com>
2022-04-06 11:57:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ f.close()
 
 #do git commit
 os.system("git config user.email 'two00434@hotmail.com'")
-os.system("git config user.name 'two00434'")
+os.system("git config user.name two00434")
 os.system("git branch new-branch")
 os.system("git checkout new-branch")
 os.system("echo 'someworks' >a.txt")
