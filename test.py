59655b09b4fb7dc934e3be80bbd9b3ad0cda88fe
zhaihotmail <zhaizhongping@hotmail.com>
2022-04-06 10:06:05 -0700

    add boto3

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("this is from github code")
+import json
+import boto3
+import re
+import time
+import os
+
+session = boto3.Session(
+    aws_access_key_id="AKIAVY7FBWM6ZVW6LFHP",
+    aws_secret_access_key="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",
+    region_name="us-east-2"
+)
+
+s3_client = session.client("s3")
+s3=session.resource("s3")
+obj=s3.Object("zhai1976","us_state/sjc.csv")
+ccs_str=obj.get()["Body"].read().decode("utf-8")
+print(ccs_str)
